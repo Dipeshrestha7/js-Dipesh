c2763c4d8a93e5dadbcfad05364f39c728469810
Dipesh Shrestha <139684493+Dipeshrestha7@users.noreply.github.com>
2023-12-06 14:38:45 +0000

    change

---
+++ 01_basics/04_String.js
@@ -0,0 +1,18 @@
+const name= "Dipesh"
+const repo= 10
+
+console.log(name + " "+ repo + " VAlue")
+
+const gameName = new String("DIpeshAxel")
+
+console.log(gameName[0])
+console.log(gameName.toLowerCase())
+console.log(gameName.toUpperCase())
+console.log(gameName.length)
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf("l"))
+
+
+console.log(gameName.substring(0,4))
+console.log(gameName.slice(0,4))
+console.log(gameName.slice(4,8))
