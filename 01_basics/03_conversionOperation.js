b3cc413f4e2e09927891ca455347976b3b7831ee
Dipesh Shrestha <139684493+Dipeshrestha7@users.noreply.github.com>
2023-12-06 11:58:45 +0000

    config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33"
+let score = "33"// null=> 0, undefined=> NaN, true,false=> 1/0 
 
 console.log(typeof score);
 
@@ -6,6 +6,13 @@ let valueInNumber = Number(score)
 
 console.log(typeof valueInNumber);
 
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
 
+let isLoggedIn = 1
+let booleanIsLoggedIn = Booleans(isLoggedIn)
 
+//" " =>false
+//"Dipesh" => true
 
