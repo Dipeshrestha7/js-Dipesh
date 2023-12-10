8f6ae3e0d5ecabcdf5a674b72382e050f6ce1067
Dipesh Shrestha <139684493+Dipeshrestha7@users.noreply.github.com>
2023-12-10 13:08:27 +0000

    config

--- 01_basics/10_scops.js
+++ 01_basics/10_scops.js
@@ -8,9 +8,52 @@ if(true){
     let a= 10
 //const b=20
      //c= 30
-     console.log("Inner",a)
+    // console.log("Inner",a)
 }
 
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)
+
+function one(){
+    const username= 'Dipesh'
+
+    function two(){
+        const website ='yotube'
+       // console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+//one()
+
+if(true){
+    const username="dipesh"
+    if(username==="dipesh"){
+        const website="yotube"
+        console.log(username+website)
+    }
+
+   // console.log(website) error cze it is outside the if statement
+}
+
+//console.log(username)  error cze it is outside the if statement
+
+/************************ Interesting **************************/
+console.log(addone(5)) //output will come
+function addone(num){
+    return num+1
+}
+
+/////////////////////////////////////////////
+
+console.log(addtwo(5)) //output won't come
+const addtwo = function(num){
+return num + 2
+}
+
+
+
+
