d3ecb88d3cc9d04a70f7f1747186cfb0d1e6bf0c
Dipesh Shrestha <139684493+Dipeshrestha7@users.noreply.github.com>
2023-12-11 08:21:05 +0000

    config

---
+++ 01_basics/11_arrow.js
@@ -0,0 +1,31 @@
+const user= {
+    username: "Jack",
+    price : "1000",
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to webbsite`)
+        console.log(this)
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username="Shyam"
+// user.welcomeMessage()
+//console.log(this)
+
+
+// function one(){
+//     let username="Dipesh"
+//     console.log(this.username)   ////////thisn cand be used in function
+// }
+
+// one()
+
+// const one= (num1,num2) => (num1+ num2)
+
+const one= (num1,num2) => ({username:'Dipesh'})
+
+console.log(one(2,3))
+
+const  myArray =[2,3,4,5,6]
