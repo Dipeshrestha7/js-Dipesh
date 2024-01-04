05e5cb61a103aa0585f61f997cc1b236f4fd53ac
Dipesh Shrestha <139684493+Dipeshrestha7@users.noreply.github.com>
2024-01-04 12:04:05 +0000

    config

---
+++ 01_basics/16_oop.js
@@ -0,0 +1,37 @@
+const user= {
+    username: "Dipesh",
+    loginCount :8,
+    isloggedIn: true,
+
+
+    getUserDetails: function(){
+        console.log("Got User Details from database");
+        console.log(`Username : ${this.username}`);
+        console.log(this);
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this)
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount= loginCount;
+    this.isLoggedIn= isLoggedIn;
+
+    return this;
+}
+
+const userOne = new User("Dipesh", 8, true)
+const userTwo = new User("Axel", 10, false)
+
+console.log(userOne)
+console.log(userTwo)
+
+
+
+
+
+
+
