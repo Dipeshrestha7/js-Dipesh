13fa6126b342462a91639463fa814d5b2db1c17e
Dipesh Shrestha <139684493+Dipeshrestha7@users.noreply.github.com>
2025-06-14 03:55:53 +0000

    common change

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,17 +1,33 @@
-const accountId= 12345;
-let accontEmail= "dipstha061@gmail.com";
-var accontPassowrd= "9876";
-accountCity= "Gorkha";
-let  accontState;
-// accountId= 2 not allowed because of const
+// const accountId= 12345;
+// let accontEmail= "dipstha061@gmail.com";
+// var accontPassowrd= "9876";
+// accountCity= "Gorkha";
+// let  accontState;
+// // accountId= 2 not allowed because of const
 
-accontEmail= "hsg@hdd.com";
-accontPassowrd= "123456";
-accountCity= "Kathmandu";
+// accontEmail= "hsg@hdd.com";
+// accontPassowrd= "123456";
+// accountCity= "Kathmandu";
 
-console.log(accountId);
-/*
-Prefer not to use var because of isse in block scope and fnctional scope
-*/
+// let a
 
-console.table([accontEmail,accontPassowrd,accountCity,accontState]);
+// console.log(accountId);
+// /*
+// Prefer not to use var because of isse in block scope and fnctional scope
+// */
+
+// // console.table([accontEmail,accontPassowrd,accountCity,accontState]);
+
+
+const id = 1
+
+// console.log(id)
+
+var email = "dip@.com"
+var email = ".com"
+
+// console.log(email)
+
+let pass = "12345#"
+let pass = "12"
+// console.log(pass)
